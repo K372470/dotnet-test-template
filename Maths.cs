fadf57990c78c3f1fbacf04b67e8d3f509cf3130
Keshka <74170358+K372470@users.noreply.github.com>
2023-08-14 19:36:56 +0300

    Update Maths.cs
    what an idiot didnt not format that file?

--- Maths.cs
+++ Maths.cs
@@ -7,7 +7,7 @@ namespace CoolSoftware.Functions
     public static float Divide(int input1, int input2) => (float)input1 / input2;
     public static int Subtract(int input1, int input2) => input1 - input2;
     public static int Add(int input1, int input2) => input1 + input2;
-        public static int Pow(int input, int power)
+    public static int Pow(int input, int power)
         {
             var result = 1;
             for (int i = 0; i < power; i++)
