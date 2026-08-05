f227caa40128c88406b83dc6c13108d505ab4e57
Keshka <K372470@users.noreply.github.com>
2026-08-06 01:42:27 +0300

    Upgrade to .NET8

--- Tests.cs
+++ Tests.cs
@@ -1,4 +1,4 @@
-using System;
+
 using CoolSoftware.Functions;
 using NUnit.Framework;
 
@@ -10,17 +10,24 @@ namespace CoolSoftware.Tests
     [Test]
     public void Is_Pow_Correct()
     {
-      Assert.AreEqual(4, Maths.Pow(2, 2), "Pow didnt work");
+      Assert.That(Maths.Pow(2, 2), Is.EqualTo(4), "Pow didnt work");
     }
     [Test]
     public void Is_Square_Correct()
-    {//ya yebal
-      Assert.AreEqual(5, Maths.Square(2), "square of 2 is 4!!!");
+    {
+      Assert.That(Maths.Square(2), Is.Not.EqualTo(5), "square of 2 is 4!!!");
     }
     [Test]
     public void Is_Multiplying_Correct()
     {
-      Assert.AreNotEqual(6, Maths.Multiply(3, 3), "3*3 should not be 6!");
+      Assert.That(Maths.Multiply(3, 3), Is.Not.EqualTo(6), "3*3 should not be 6!");
+    }
+    [TestCase(0,0,0)]
+    [TestCase(0,1,1)]
+    [TestCase(2,3,5)]
+    public void Test_Add(int a,int b,int sum)
+    {
+      Assert.That(Maths.Add(a, b), Is.EqualTo(sum));
     }
   }
 }
